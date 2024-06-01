098053bfeeffb45c16592904028beaad6484122c
tenggono98 <45934316+tenggono98@users.noreply.github.com>
2024-06-02 00:43:36 +0700

    NPM BUILD

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/css/app-admin.css',
+                'resources/js/datepicker.js'
             ],
             refresh: true,
         }),
